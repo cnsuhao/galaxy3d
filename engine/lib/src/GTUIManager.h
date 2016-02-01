285f22a5ac3f885d35ebf5873abd25c17a17ddf0
47324322qqcom <47324322qqcom@daa9cee9-2eb3-44c5-b6f2-30917026f99e>
2016-02-01 15:38:52 +0000

    ###改进 * 增加GTUIManager.LoadFont接口。
    ###修复
    * World.Done中加入GTUIManager.Done以释放缓存atlas。

--- engine/lib/src/GTUIManager.h
+++ engine/lib/src/GTUIManager.h
@@ -11,6 +11,7 @@ namespace Galaxy3D
     {
     public:
         static void Done();
+        static void LoadFont(const std::string &name, const std::string &file);
         static std::shared_ptr<UIAtlas> LoadAtlas(const std::string &name, const std::string &file);
         static std::shared_ptr<GameObject> LoadWindowFromJsonFile(const std::string &file, int sorting_layer, float pixel_per_unit);
         static std::shared_ptr<UIAtlas> FindAtlas(const std::string &name);
